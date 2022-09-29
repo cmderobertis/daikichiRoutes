29374c8e7a1727638cdcbc81a2d4bd504e0514ef
cmderobertis <cmderobertis@gmail.com>
2022-09-29 16:18:25 -0700

    added routes with PathVariables

--- src/main/java/net/cmderobertis/daikichiroutes/DaikichiController.java
+++ src/main/java/net/cmderobertis/daikichiroutes/DaikichiController.java
@@ -1,5 +1,6 @@
 package net.cmderobertis.daikichiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +21,15 @@ public class DaikichiController {
     public String tomorrow() {
         return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
     }
+
+    @RequestMapping("/travel/{city}")
+    String travel(@PathVariable String city) {
+        return "Congratulations! You will soon travel to " + city + "!";
+    }
+
+    @RequestMapping("/lotto/{number}")
+    String lotto(@PathVariable int number) {
+        return number % 2 == 0 ? "You will take a grand journey in the near future, but be weary of tempting offers." : "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+    }
+
 }
